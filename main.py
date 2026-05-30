00749989669a0aed85a07a20c9ab46a65020506f
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 08:42:30 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class searchFncryptWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = searchFncryptWorker()
+    print("Worker engine initialized.")
